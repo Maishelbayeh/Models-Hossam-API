e409629b7280a14cefa2e77f66a26c154611530c
MaiShelbayeh <s11923686@najah.stu.edu>
2025-02-02 15:00:29 +0200

    extract text

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from resume_parser import parse_resume
-from career_model import CareerPathRecommender
+# from career_model import CareerPathRecommender
 
 # Load environment variables
 load_dotenv()
@@ -20,20 +20,20 @@ if uploaded_file:
         # Parse the resume
         st.write("Parsing the resume...")
         resume_data = parse_resume(uploaded_file)
-        st.success("Resume parsed successfully!")
+        st.success(resume_data)
         
         # Initialize the recommender
-        recommender = CareerPathRecommender()
+        # recommender = CareerPathRecommender()
 
-        # Get recommendations
-        st.write("Generating career path recommendations...")
-        career_paths = recommender.get_recommendations(resume_data)
+    #     # Get recommendations
+    #     st.write("Generating career path recommendations...")
+    #     career_paths = recommender.get_recommendations(resume_data)
         
-        if isinstance(career_paths, str):
-            st.success("Recommendations:")
-            st.write(career_paths)
-        else:
-            st.error("No recommendations available.")
+    #     if isinstance(career_paths, str):
+    #         st.success("Recommendations:")
+    #         st.write(career_paths)
+    #     else:
+    #         st.error("No recommendations available.")
     except Exception as e:
         st.error(f"An error occurred: {e}")
 else:
